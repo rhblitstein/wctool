628348578ca88df3382ec082a0473d5d3982ff3b
rhblitstein <rhblitstein@gmail.com>
2025-12-02 15:03:50 -0700

    establish flags

--- main.go
+++ main.go
@@ -1,10 +1,39 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
+	"log"
+	"os"
+
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
-	fmt.Println(bytes.NewReader([]byte("Test")).Len())
+	app := &cli.App{
+		Name:  "wctool",
+		Usage: "A command line tool challenge",
+		Flags: []cli.Flag{
+			&cli.BoolFlag{
+				Name:    "c",
+				Usage:   "Return number of bytes in text",
+				Aliases: []string{"C"},
+			},
+		},
+		Action: func(c *cli.Context) error {
+			if c.Bool("c") {
+				runC()
+			} else {
+				fmt.Println("Invalid flag")
+			}
+			return nil
+		},
+	}
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func runC() {
+	fmt.Print("test")
 }
